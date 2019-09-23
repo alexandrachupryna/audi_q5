0346bb865fc16b1792dc73444a0e284e60bc56cf
alexandrachupryna <alexandrachuprina93@gmail.com>
2019-09-23 17:28:24 +0300

    Updates

---
+++ precache-manifest.595f12ddd4cbb05e6fc8b20eb861f0d1.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "16593928a66d877ef162f43f0e3e35b1",
+    "url": "/audi_q5/index.html"
+  },
+  {
+    "revision": "da3da03b89c7eaa2d31e",
+    "url": "/audi_q5/static/css/main.1e41e86a.chunk.css"
+  },
+  {
+    "revision": "cdafae09e3ccfb5fd513",
+    "url": "/audi_q5/static/js/2.0f26476d.chunk.js"
+  },
+  {
+    "revision": "da3da03b89c7eaa2d31e",
+    "url": "/audi_q5/static/js/main.40018c55.chunk.js"
+  },
+  {
+    "revision": "539ee19a496e44654c49",
+    "url": "/audi_q5/static/js/runtime-main.ef2025e5.js"
+  },
+  {
+    "revision": "96dab781af57a73e14435b1f3112255e",
+    "url": "/audi_q5/static/media/AudiTypeScreen-Bold.96dab781.woff"
+  },
+  {
+    "revision": "c48ef1fc825a9b52da9bff7fb2e318b0",
+    "url": "/audi_q5/static/media/AudiTypeScreen-Bold.c48ef1fc.ttf"
+  },
+  {
+    "revision": "deb638bd93aee79b5ea7ba0f9be60e73",
+    "url": "/audi_q5/static/media/AudiTypeScreen-Bold.deb638bd.eot"
+  },
+  {
+    "revision": "6890e6fade5253987ed3a29508fbce70",
+    "url": "/audi_q5/static/media/AudiTypeScreen-Light.6890e6fa.woff"
+  },
+  {
+    "revision": "8d411e950ca8fb0476bb931f7a6a06dd",
+    "url": "/audi_q5/static/media/AudiTypeScreen-Light.8d411e95.eot"
+  },
+  {
+    "revision": "e75227f1d409a928d38c693b9d68b7f7",
+    "url": "/audi_q5/static/media/AudiTypeScreen-Light.e75227f1.ttf"
+  },
+  {
+    "revision": "3c63b9aa7064303467fed842b6814e7b",
+    "url": "/audi_q5/static/media/AudiTypeScreen-Normal.3c63b9aa.woff"
+  },
+  {
+    "revision": "b25753f14f3c25db932df1e76ddd0753",
+    "url": "/audi_q5/static/media/AudiTypeScreen-Normal.b25753f1.ttf"
+  },
+  {
+    "revision": "b4b4b24e645dc171375c42c982af30d1",
+    "url": "/audi_q5/static/media/AudiTypeScreen-Normal.b4b4b24e.eot"
+  },
+  {
+    "revision": "eb406f83945f25c6f8aeb35b67036210",
+    "url": "/audi_q5/static/media/Layer3.eb406f83.png"
+  },
+  {
+    "revision": "6be640a29d5d2fd003826e25f2beee5e",
+    "url": "/audi_q5/static/media/Shadow1.6be640a2.png"
+  },
+  {
+    "revision": "1067d53974c28a0922994007b92e2ec8",
+    "url": "/audi_q5/static/media/Shadow2.1067d539.png"
+  },
+  {
+    "revision": "3ac6a92e061aebeb4c86bc5481e48549",
+    "url": "/audi_q5/static/media/auto.3ac6a92e.png"
+  }
+]);
